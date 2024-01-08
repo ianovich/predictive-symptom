5ebe03ffab66fbe67d9a448438c89a53c1c92155
ianovich <imunyasya22@gmail.com>
2024-01-08 13:14:44 +0300

    updated SignUp1

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,13 +1,60 @@
-import React from 'react'
-
+import React from "react";
+import signupImg from "../assets/images/signup.png";
 const SignUp = () => {
-  return <section className='px-5 xl:px-0'>
-    <div className='max-w-[1170px] mx-auto'>
-<div className='grid grid-cols-1'></div>
+  return (
+    <section>
+      <div className="max-w-[1170px] mx-auto">
+        <div className="grid grid-cols-1 lg:grid-cols-2">
+          {/*----img box---- */}
+          <div className="hidden lg:block ">
+            <figure>
+              <img src={signupImg} />
+            </figure>
+          </div>
+          {/*----sign up-----*/}
+          <div className="rounded-l-lg lg:pl-16 py-10">
+            <h3 className="text-headingColor text-[22px] leading-9 font-bold mb-10">
+              Create an<span className="text-primaryColor">Account</span>
+            </h3>
+            <form>
+              <div>
+                <input
+                  type="text"
+                  placeholder="Full Name"
+                  name="name"
+                  value=""
+                  className="w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[22px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer"
+                  required
+                />
+              </div>
+              <div>
+                <input
+                  type="email"
+                  placeholder="Enter Your Email"
+                  name="email"
+                  value=""
+                  className="w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[22px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer"
+                  required
+                />
+              </div>
+              <div>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value=""
+                  className="w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[22px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer"
+                  required
+                />
+              </div>
 
-    </div>
-   
+              <div></div>
+            </form>
+          </div>
+        </div>
+      </div>
     </section>
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
