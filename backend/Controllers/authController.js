2b807be49c8e5243d32c48add91cf622caff6629
ianovich <imunyasya22@gmail.com>
2024-01-08 19:22:21 +0300

    authController

---
+++ backend/Controllers/authController.js
@@ -0,0 +1,14 @@
+export const register =async(req,res)=>{
+try {
+  
+} catch (err) {}
+
+};
+
+export const login =async(req,res)=>{
+  try {
+    
+  } catch (err) {}
+  
+  };
+  
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,50 @@
-import express from "express"
-import cookieParser from "cookie-parser"
-import cors from 'cors'
-import mongoose from "mongoose"
-import dotenv from 'dotenv'
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from 'cors';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 dotenv.config()
+
+const app =express()
+const port= process.env.PORT || 8000
+
+const corsOptions={
+ origin:true
+}
+app.get('/',(req,res)=>{
+  res.send('Api is working')
+});
+
+
+//db connection
+mongoose.set('strictQuery',false)
+const connectDB= async()=>{
+  try {
+    await mongoose.connect(process.env.MONGO_URL,{
+   useNewUrlParser:true,
+   useUnifiedTopology:true,
+
+    })
+console.log('MongoDB database is connected')
+
+  } catch (err) {
+   
+    console.log('MongoDB database connection failed');
+
+  }
+}
+
+//Middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions)); 
+
+
+
+
+app.listen(port, ()=>{
+connectDB();
+console.log('Server is running on port' + port)
+
+});
