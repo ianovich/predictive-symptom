758af6d1031464a0fefeb466c64493302bff59d2
ianovich <imunyasya22@gmail.com>
2024-01-08 10:19:19 +0300

    updated SignUp

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
+  return <section className='px-5 xl:px-0'>
+    <div className='max-w-[1170px] mx-auto'>
+<div className='grid grid-cols-1'></div>
+
+    </div>
+   
+    </section>
 }
 
 export default SignUp
